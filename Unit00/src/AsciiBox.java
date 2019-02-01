33f1f42d40e44f7a39fa590d992db2be60397ddd
NunuStein <nunustein@gmail.com>
2019-02-01 11:12:31 -0800

    Update AsciiBox.java

--- Unit00/src/AsciiBox.java
+++ Unit00/src/AsciiBox.java
@@ -1,17 +1,31 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Nicole Stein
+//Date -  2/1/2019
+//Class - APCSA p2
+//Lab  - AsciiBox
 
 public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
+		System.out.println("Nicole Stein \t  2/1/19 \n\n" );
+		
 		System.out.println("+++++++++++++++++++++++++ " );
-
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
 	}
 }
