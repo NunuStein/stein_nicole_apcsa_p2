8453b4fffa86ed3a2a970712bea586927dae3f62
NunuStein <nunustein@gmail.com>
2019-02-01 11:12:23 -0800

    Update Variables.java

--- Unit00/src/Variables.java
+++ Unit00/src/Variables.java
@@ -11,16 +11,26 @@ public class Variables
 	public static void main ( String[] args )
 	{
 		//define 1 variable of each of the
-		//following data types
+		//following data type		
 		//byte		short		int 		long
 		//float		double
 		//char      boolean		String
+		byte x = 12;
+		short y = 14;
+		int z = 46;
+		long a = 52;
+		float b = 6;
+		double c = 5;
+		char cher = "a";
+		boolean yes = true;
+		String nope = "nope";
 
 		//integer variables
 		byte byteOne = 127;
 
 
 		//decimal variables
+		
 
 
 		//other integer types
